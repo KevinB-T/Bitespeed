1796629f432f69fc47726972abfc7a66d287ba98
KevinB-T <kevinbiju03@gmail.com>
2025-05-28 20:34:21 +0530

    fix:

--- app.py
+++ app.py
@@ -145,4 +145,4 @@ def identify():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5432, debug=True)
